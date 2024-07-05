ee1d341a82d85e9b70c5b0f9ca34ffe3c452fbe2
chariottenoufoho <150136669+chariottenoufoho@users.noreply.github.com>
2024-07-05 17:22:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
 
+app = FastAPI()
+# Allow all origin
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+)
+
+class Number(BaseModel):
+    num1: float
+    num2: float
+
+
+@app.post('/prod')
+async def welcome(num: Number):
+    a = num.num1
+    b = num.num2
+    print(f"{a} * {b} = {int(a*b)}")
+    return{f"Request confirmation: prod is {a*b}!"}
+
+if __name__=='__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8802)
+    
